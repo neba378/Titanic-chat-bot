337b255497e4a5e369d6b476138f88009c3bb1a4
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2025-02-24 09:33:47 +0300

    edit requirement for backend and seome cleanups

--- backend/agent.py
+++ backend/agent.py
@@ -1,14 +1,15 @@
-from dotenv import load_dotenv
-from langchain_google_genai import GoogleGenerativeAI
-from langchain_experimental.agents import create_pandas_dataframe_agent
-import pandas as pd
-import os
-import base64
-import matplotlib.pyplot as plt
+# Standard library imports
 import io
-import spacy
+import base64
+
+# Third-party imports
+from dotenv import load_dotenv
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
 
 # Load environment variables
 load_dotenv()
@@ -22,8 +23,6 @@ gemini_model = GoogleGenerativeAI(model="gemini-pro", temperature=0.8)
 # Create LangChain agent
 agent = create_pandas_dataframe_agent(gemini_model, df, verbose=True, allow_dangerous_code=True)
 
-# Load NLP models
-nlp = spacy.load("en_core_web_sm")
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")  # Small, fast embedding model
 
 # Dataset column descriptions (for better understanding)
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI
 from agent import process_query
 from schemas import Query
 
@@ -9,7 +9,6 @@ def ask_question(query: Query):
     response, chart = process_query(query.question)
     return {"answer": response, 'chart': chart}
 
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
